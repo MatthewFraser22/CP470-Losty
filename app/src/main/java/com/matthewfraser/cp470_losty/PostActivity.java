4886cab07f670ae7351649b8a3cb5b05f8544e5b
Matthew Fraser <52222907+MatthewFraser22@users.noreply.github.com>
2022-11-05 21:35:59 -0400

    Backend code
    - SQLiteDatabase
    - backend logic

--- app/src/main/java/com/matthewfraser/cp470_losty/PostActivity.java
+++ app/src/main/java/com/matthewfraser/cp470_losty/PostActivity.java
@@ -1,14 +1,125 @@
 package com.matthewfraser.cp470_losty;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class PostActivity extends AppCompatActivity {
+    protected static String TAG = "PostActivity";
+    private final int REQUEST_CODE = 123;
+
+    EditText itemNameEditText, brandNameEditText, colorEditText,
+            descriptionEditText, otherEditText;
+    ImageButton imageButton;
+    Button postButton;
+    PostDatabaseHelper postDatabase;
+    String lostItemPhotoString = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        itemNameEditText = findViewById(R.id.lost_item_edit_text);
+        brandNameEditText = findViewById(R.id.lost_item_brand_edit_text);
+        colorEditText = findViewById(R.id.lost_item_color_edit_text);
+        descriptionEditText = findViewById(R.id.lost_item_description_edit_text);
+        otherEditText = findViewById(R.id.lost_item_other_edit_text);
+        postButton = findViewById(R.id.postButton);
+        imageButton = findViewById(R.id.imageButton);
+
+        postDatabase = new PostDatabaseHelper(this);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent photoGalleryintent = new Intent();
+                photoGalleryintent.setType("image/*");
+                photoGalleryintent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(photoGalleryintent, "Select Picture"), REQUEST_CODE);
+            }
+        });
+
+        postButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Boolean isPostValid = isPostValid();
+
+                if (isPostValid == true) {
+                    String name = itemNameEditText.getText().toString();
+                    String brand = brandNameEditText.getText().toString();
+                    String color = colorEditText.getText().toString();
+                    String description = descriptionEditText.getText().toString();
+                    String other = otherEditText.getText().toString();
+
+                    Boolean insertData = postDatabase.addData(lostItemPhotoString, name, brand, color, description, other);
+
+                    if (insertData == true) {
+                        Log.i("TAG", "TESTING - data saved");
+
+                        // 2. display a loading symbol
+                        // 3. Save Display a snackbar saying the post was completed successfully
+                        // 4. close the activity and move to the post "main page"
+                    } else {
+                        Log.i("TAG", "TESTING - data NOT saved");
+                        // snack or toast showing error
+                    }
+
+                }
+            }
+        });
+    }
+
+    private Boolean isPostValid() {
+        Boolean name = itemNameEditText.getText().toString().isEmpty();
+        Boolean brand = brandNameEditText.getText().toString().isEmpty();
+        Boolean color = colorEditText.getText().toString().isEmpty();
+        Boolean description = descriptionEditText.getText().toString().isEmpty();
+
+        Boolean isFieldEmpty = name && brand && color && description && lostItemPhotoString.isEmpty() == false;
+
+        if (isFieldEmpty == true) {
+
+            // TODO - Custom alert dialog box
+            Log.i(TAG, "Post is NOT valid");
+            return false;
+        } else {
+            Log.i(TAG, "Post is valid");
+            return true;
+        }
+    }
+
+    // TESTING Diplay
+    public void display(String title, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_CODE) {
+                Uri selectedImageUri = data.getData();
+                if (null != selectedImageUri) {
+                    lostItemPhotoString = selectedImageUri.toString();
+                    imageButton.setImageURI(selectedImageUri);
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/matthewfraser/cp470_losty/PostDatabaseHelper.java
@@ -0,0 +1,68 @@
+package com.matthewfraser.cp470_losty;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class PostDatabaseHelper extends SQLiteOpenHelper {
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "post.db";
+    public static final String TABLE_NAME = "post_table";
+    public static final String COL1 = "ID";
+    public static final String COL2 = "PICTURE";
+    public static final String COL3 = "ITEM";
+    public static final String COL4 = "BRAND";
+    public static final String COL5 = "COLOR";
+    public static final String COL6 = "DESCRIPTION";
+    public static final String COL7 = "OTHER";
+
+    public PostDatabaseHelper(Context cxt) {
+        super(cxt, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTable = "CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + "PICTURE TEXT, ITEM TEXT, BRAND TEXT, COLOR TEXT, DESCRIPTION TEXT, OTHER TEXT)";
+
+        db.execSQL(createTable);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean addData(String picture, String item, String brand, String color, String description, String other) {
+        SQLiteDatabase database = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(COL2, picture);
+        contentValues.put(COL3, item);
+        contentValues.put(COL4, brand);
+        contentValues.put(COL5, color);
+        contentValues.put(COL6, description);
+        contentValues.put(COL7, other);
+
+        long result = database.insert(TABLE_NAME, null, contentValues);
+
+        if (result == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public Cursor showData() {
+        SQLiteDatabase database = this.getWritableDatabase();
+        Cursor data = database.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+
+        return data;
+
+    }
+}
