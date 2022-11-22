a822720a99bbef8a413562100e2f6057e07e0499
news5180 <113859625+news5180@users.noreply.github.com>
2022-11-22 13:45:51 -0500

    account creation completed (#2)
    * account creation completed
    
    * login completed

---
+++ app/src/main/java/com/matthewfraser/cp470_losty/Login.java
@@ -0,0 +1,65 @@
+package com.matthewfraser.cp470_losty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+
+public class Login extends AppCompatActivity {
+    private Button createAccount;
+    private Button login;
+    private EditText editUsername;
+    private EditText editPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        editUsername = (EditText)findViewById(R.id.enterLoginUsername);
+        editPassword = (EditText)findViewById(R.id.enterLoginPassword);
+        createAccount = (Button)findViewById(R.id.accountCreationButton);
+        login = (Button)findViewById(R.id.loginButton);
+
+
+        DBHandler db = new DBHandler(Login.this);
+
+        ArrayList<AccountModel> accounts = db.readAccounts();
+
+        for(int i = 0; i < accounts.size(); i++) {
+            System.out.println(accounts.get(i).getUsername());
+        }
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = editUsername.getText().toString();
+                String password = editPassword.getText().toString();
+                for(int i = 0; i < accounts.size(); i++) {
+                    if(accounts.get(i).getUsername().equals(username)) {
+                        if(accounts.get(i).getPassword().equals(password)) {
+                            Intent intent = new Intent(Login.this, TestActivity.class);
+                            startActivity(intent);
+                        }
+                    }
+                }
+
+            }
+        });
+
+
+        createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, AccountCreation.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/matthewfraser/cp470_losty/TestActivity.java
+++ app/src/main/java/com/matthewfraser/cp470_losty/TestActivity.java
@@ -2,16 +2,13 @@ package com.matthewfraser.cp470_losty;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class TestActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
+        setContentView(R.layout.activity_test);
     }
-
 }
